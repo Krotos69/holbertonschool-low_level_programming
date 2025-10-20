c716fef8970fb4098dc26833ae89120e007fb668
eugenio martinez <eumartinez@LAPTOP-45I9DFOR.localdomain>
2025-10-20 11:19:21 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc puts_4-puts.c -o 4-puts
+#include <stdio.h>
+
+/**
+ * writes a program that print programming is like building a multilingual puzzle
+ *
+ * hello,puts
+ */
+int main(void)
+{
+	puts("\"programming is like building a mulilingual puzzle");
+	return (0);
