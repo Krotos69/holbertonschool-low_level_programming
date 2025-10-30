f023dd477889462b4bd8c5817b65a9d65f623f7b
eugenio martinez <emartinez714@gmail.com>
2025-10-30 14:52:26 -0400

    3-print_alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * File:   3-print_alphabets.c 
+*/ 
+
+int main(void)
+{
+	char ch;
+
+	/* Prints the alphabet in lowercase, and then in uppercase*/
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	/* Prints the alphabet in lowercase, and then in uppercase*/
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n'); /* New line at the end */
+	return 0;
+}
