7fe1143c15a1daa170eed17cd8947d3230b9edbb
eugenio martinez <emartinez714@gmail.com>
2025-10-30 17:13:30 -0400

    5-print_numbers.c output correction logic

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,11 +6,12 @@
  * Return: Always 0.
  */
 
-int main(void)
+int main()
 {
 	int num;
 
-	for (num = 0; num < 10; num++) 
-		printf("%d\n", num);
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
 	return (0);
 }
