a291aa387542d88e99e2d09a5b6512286713c1ac
eugenio martinez <emartinez714@gmail.com>
2025-10-30 14:21:04 -0400

    2-print_alphabet.c betty2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,13 @@
  * main - prints the alphabet in lowercase
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
+
 	putchar('\n');
 	return (0);
 }
