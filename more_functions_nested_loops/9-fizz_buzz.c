56190c293ef0b90becb88d891cfca1b013ff2e3f
eugenio martinez <emartinez714@gmail.com>
2025-11-06 14:45:12 -0400

    9-fizz_buzz.c 3

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 /**
  * main - prints the numbers from 1 to 100
- * 
+
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++) 
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
