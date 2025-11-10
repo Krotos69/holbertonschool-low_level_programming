ac0a5d5c9599c2190a5deab0bff220e6f0bd819c
eugenio martinez <emartinez714@gmail.com>
2025-11-10 13:09:50 -0400

    main.h fix comment

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 int _putchar(int c);
-void reset_to_98(int *n);//* reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
-void swap_int(int *a, int *b);// swap_int - swaps the values of two integers
-int _strlen(char *s); // _strlen - returns the length of a string
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s); 
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
