828b1007fc8e6855dfd343cbcb999d333defab94
eugenio martinez <emartinez714@gmail.com>
2025-11-10 19:56:44 -0400

    8-print_array.c betty

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-/** 
+/**
  * print_array - prints n elements of an array of integers
  * @a: pointer to the first element of the array
  * @n: number of elements to print
@@ -8,6 +8,7 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		if (i != n - 1)
