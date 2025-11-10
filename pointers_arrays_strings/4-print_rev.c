6ef88816ef47b8919a3b1d357f61b5505e5232e4
eugenio martinez <emartinez714@gmail.com>
2025-11-10 15:12:44 -0400

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+	while (s[len] != '\0')
+		len++;
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
