a2cb429a6cec223f5fa5563692689f47b2620519
eugenio martinez <emartinez714@gmail.com>
2025-11-10 16:09:38 -0400

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * if the length is odd, prints the last n characters
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (len + 1) / 2; i < len; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
