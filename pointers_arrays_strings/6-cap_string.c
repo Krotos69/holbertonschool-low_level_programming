1ed8e385d7b53d89075e8cb0ae572d77ba94a3ee
eugenio martinez <emartinez714@gmail.com>
2025-11-25 16:05:39 -0400

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,23 +10,23 @@
 char *cap_string(char *str)
 
 {
-        int i = 0;
-        char separators[] = " \t\n,;.!?\"(){}";
-        int j;
+	int i = 0;
+	char separators[] = " \t\n,;.!?\"(){}";
+	int j;
 
-    if (str[i] >= 'a' && str[i] <= 'z')
-                str[i] -= 32;
-        while (str[i] != '\0')
-        {
-                for (j = 0; separators[j] != '\0'; j++)
-                {
-                        if (str[i] == separators[j] && str[i + 1] >= 'a' && str[ i + 1] <= 'z')
-                        {
-                            str[ +1] -= 32;
-                            break;
-                        }
-                }
-                i++;
-        }
-        return (str);
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
+	while (str[i] != '\0')
+	{
+	for (j = 0; separators[j] != '\0'; j++)
+	{
+		if (str[i] == separators[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+			str[+1] -= 32;
+		break;
+			}
+		}
+		i++;
+	}
+		return (str);
 }
