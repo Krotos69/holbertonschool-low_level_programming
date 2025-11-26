68069738e8c8ac8618457e1bded833c607cf9530
eugenio martinez <emartinez714@gmail.com>
2025-11-26 17:05:12 -0400

    6-cap_string.c code review

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,27 +6,30 @@
  * @str: the string to modify.
  *
  * Return: A pointer to the modified string.
- */
+*/
 char *cap_string(char *str)
-
 {
-	int i = 0;
+	int i = 0, j;
 	char separators[] = " \t\n,;.!?\"(){}";
-	int j;
 
-	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
 	while (str[i] != '\0')
 	{
-	for (j = 0; separators[j] != '\0'; j++)
-	{
-		if (str[i] == separators[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= 32;
+		}
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
 			{
-			str[+1] -= 32;
-		break;
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+				break;
 			}
 		}
 		i++;
 	}
-		return (str);
+	return (str);
 }
