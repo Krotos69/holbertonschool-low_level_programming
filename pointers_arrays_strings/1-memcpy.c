e05fc32645922b6331e57f38a3fb1d755043dd32
eugenio martinez <emartinez714@gmail.com>
2025-11-26 18:59:20 -0400

    1-memcpy.c betty

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 
