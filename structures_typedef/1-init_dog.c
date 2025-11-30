72d48d20f564fb44f81341453a757e346fa9886f
eugenio martinez <emartinez714@gmail.com>
2025-11-30 19:11:51 -0400

    1-init_dog.c betty

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,6 +7,11 @@
 * @d: Pointer to the name to assign.
 * @age: age to assing.
 * @owner: pointer to the owner's name to assing.
+*@name: name to assign to the dog.
+*
+*Description: this function takes a pointer to a struct dog and
+* initializes its fields with the provided values. If the pointer is NULL,
+* the function does nothing.
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
