df2792cb7598088500691b6ab4f2cbfd57cee923
eugenio martinez <emartinez714@gmail.com>
2025-11-30 19:21:15 -0400

    1-init_dog.c betty1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,12 +2,12 @@
 #include <stddef.h>
 #include <stdio.h>
 
-/**
-* @init_dog: Initializes a variable of type struct dog.
-* @d: Pointer to the name to assign.
-* @age: age to assing.
-* @owner: pointer to the owner's name to assing.
-*@name: name to assign to the dog.
+/*
+* init_dog - Initializes a variable of type struct dog
+* @d: Pointer to the name to assign
+* @age: age to assing
+* @owner: pointer to the owner's name to assing
+*@name: name to assign to the dog
 *
 *Description: this function takes a pointer to a struct dog and
 * initializes its fields with the provided values. If the pointer is NULL,
