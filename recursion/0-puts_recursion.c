dd917f9f24c27dac307ec01191f0d42a3febad31
eugenio martinez <emartinez714@gmail.com>
2025-12-07 21:43:53 -0400

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Prints a string followed by a newline.
+ * @s: The string to be printed
+ *
+ * Description: Recursively prints each char in the string
+ * until the null terminator is reached, then prints a newline.
+ * Mimics the behavior of the standard puts function.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+		_putchar(*s);
+		_puts_recursion(s + 1);
+}
