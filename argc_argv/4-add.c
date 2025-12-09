d1cd90a05aea968fd95422fd781617e64a8d6242
eugenio martinez <emartinez714@gmail.com>
2025-12-08 21:56:21 -0400

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+*is number - checks if a string is a valid number
+*@s: string to check
+*Return: 1 if valid number, 0 otherwise
+*/
+int is_number(char *str)
+{
+	int i = 0;
+
+	if (str[0] == '-' || str[0] == '+') /* handle negative and positive signs */
+		i++;
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/***main - entry point of the program
+*@argc: argument count
+*@argv: argument vector
+*Return: 0 if successful, 1 if tan argument contains non-digit characters
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, num;
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		num = atoi(argv[i]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
