50460f392ec64626f38bbfd31970110b971fa317
eugenio martinez <emartinez714@gmail.com>
2025-12-10 13:48:54 -0400

    3-op_functions Write a program that performs simple operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of a and b
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+int op_add(int a, int b)
+
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - difference of a and b
+ * @a: first int
+ * @b: second int
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - division of a by b
+ * @a: first int
+ * @b: second int
+ * Return: division result
+ */
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * op_mod - remainder of division of a by b
+ * @a: first int
+ * @b: second int
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
