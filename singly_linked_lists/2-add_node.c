5cfddafaf3159c0a87d49c10459dfc80211c4949
eugenio martinez <emartinez714@gmail.com>
2025-12-12 15:03:50 -0400

    betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@
  * @str: string to be added to the new node
  * Return: address of the new element, or NULL if it failed
  */
- 
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node; /* Pointer to the new node */
