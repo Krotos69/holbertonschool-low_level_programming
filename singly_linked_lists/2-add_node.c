8dbbcb882343a6fc4e23968b90a731b6df7c033c
eugenio martinez <emartinez714@gmail.com>
2025-12-12 16:00:12 -0400

    error in str2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,6 +2,21 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
+/**
+*_strlen- calcula la longitud de una cadena
+*@s: puntero a la cadena
+*Return: longitud de la cadena
+*/
+unsigned int _strlen(const char *s)
+{
+	unsigned int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the pointer to the head of the list_t list
@@ -29,17 +44,4 @@ list_t *add_node(list_t **head, const char *str)
 	*head = new_node; /* Update the head to point to the new node */
 
 	return (new_node); /* Return the address of the new element */
-}
-/**
-*_strlen- calcula la longitud de una cadena
-*@s: puntero a la cadena
-*Return: longitud de la cadena
-*/
-unsigned int _strlen(const char *s)
-{
-	unsigned int len = 0;
-
-	while (s[len] != '\0')
-		len++;
-	return (len);
 }
