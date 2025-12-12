1a4cced960540b342338dbc7678ddc186a6eab68
eugenio martinez <emartinez714@gmail.com>
2025-12-12 14:54:14 -0400

    betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * size_list - returns the numbers of elements of a list_t list
+ * list_len - Returns the number of elements in a linked list_t list.
+ *size_list - returns the numbers of elements of a list_t list
  * @h: pointer to the head of the list_t list
  * Return: the number of nodes in the list
  */
 size_t list_len(const list_t *h)
+
 {
 	size_t count = 0; /*counter for nodes */
 
