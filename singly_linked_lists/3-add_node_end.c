4b3cb0c8a55700021441c5ed82d675eb53cc90a6
eugenio martinez <emartinez714@gmail.com>
2025-12-13 19:06:58 -0400

    fix code

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,44 +1,49 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
  * add_node_end - adds a new node at the end of a list_t list
- * @head: Double pointer to the head of the list_t list
- * @str: string to be duplicated and store in the new node
+ * @head: pointer to the pointer to the head of the list_t list
+ * @str: string to be duplicated and added to the new node
  *
  * Return: address of the new element, or NULL if it failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
-
 {
-	list_t *new_node; /* Pointer to the new node */
-	list_t *temp; /* Temporary pointer to traverse the list */
+	list_t *new_node, *temp;
 
-	new_node  = malloc(sizeof(list_t)); /* Allocate memory for the new node */
-	if (new_node == NULL) /* Check if memory allocation was successful */
-		{
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
-		}
-		new_node->str = strdup(str); /* Duplicate the string into the new node */
-	if (new_node->str == NULL) /* Check if string duplication was successful */
+
+	/* Duplicate the string */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		free(new_node); /* Free the allocated memory for the node */
+		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str); /* calcualte the length of the string */
-	new_node->next = NULL; /* Set the next pointer of the new node to NULL */
-	if (*head == NULL) /* If the list is empty, make the new node the head */
+
+	/* Set length and next pointer */
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	/* If list is empty, new node becomes head */
+	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	temp = *head; /* Initialize temp to the head of the list */
-	while (temp->next != NULL) /* Traverse to the end of the list */
-	{
+
+	/* Otherwise, traverse to the last node */
+	temp = *head;
+	while (temp->next != NULL)
 		temp = temp->next;
-	}
-	temp->next = new_node; /* Link the last node to the new node */
-	return (new_node); /* Return the address of the new element */
+
+	/* Link new node at the end */
+	temp->next = new_node;
+
+	return (new_node);
 }
