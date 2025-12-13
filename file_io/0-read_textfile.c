0254d4b185b4ab0344bd3b493ac5461f789f224c
eugenio martinez <emartinez714@gmail.com>
2025-12-13 15:29:27 -0400

    verification code

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,30 +8,28 @@
  * Return: The actual number of letters it could read and print,
  * or 0 if an error occurs.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
 
-	if (!filename)
-		return (0);
+    if (!filename)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buffer = malloc(letters);
-	if (!buffer)
-		return (0);
+    buffer = malloc(letters);
+    if (!buffer)
+        return (0);
 
-	bytes_read = read(fd, buffer, letters);
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    bytes_read = read(fd, buffer, letters);
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	free(buffer);
-	close(fd);
+    free(buffer);
+    close(fd);
 
-	return ((bytes_read == bytes_written) ? bytes_written : 0);
+    return ((bytes_read == bytes_written) ? bytes_written : 0);
 }
