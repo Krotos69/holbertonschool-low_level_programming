e1d94971fe3afda4cccd15130fbc88a79f31c0af
eugenio martinez <emartinez714@gmail.com>
2025-12-13 18:49:25 -0400

    fix betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
  * Return: address of the new element, or NULL if it failed
  */
 
- list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 
 {
 	list_t *new_node; /* Pointer to the new node */
@@ -19,7 +19,7 @@
 	new_node  = malloc(sizeof(list_t)); /* Allocate memory for the new node */
 	if (new_node == NULL) /* Check if memory allocation was successful */
 		{
-			return (NULL);
+		return (NULL);
 		}
 		new_node->str = strdup(str); /* Duplicate the string into the new node */
 	if (new_node->str == NULL) /* Check if string duplication was successful */
