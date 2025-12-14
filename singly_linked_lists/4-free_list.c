c8ebd2dd4e42c7ff9031cca079064e62348937ee
eugenio martinez <emartinez714@gmail.com>
2025-12-13 23:23:17 -0400

    4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_list - frees a list_t list
- * @head: pointer to the first node of the list
- * this funtion traverses the list and frees each node's allocated memory
-  */
+* free_list - frees a list_t list
+* @head: pointer to the first node of the list
+* this funtion traverses the list and frees each node's allocated memory
+*/
 
- void free_list(list_t *head)
+void free_list(list_t *head)
 {
 	list_t *current;
 	list_t *next_node;
+
 	current = head;
 	while (current != NULL)
 	{
@@ -17,5 +18,5 @@
 		free(current->str);
 		free(current);
 		current = next_node;
-	} 
+	}
 }
